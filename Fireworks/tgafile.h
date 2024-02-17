e0370ec7bd09e561d06c215fc34dadde75b2d21d
animesyon <121491211+animesyon@users.noreply.github.com>
2024-02-17 09:15:29 +0800

    initial commit

---
+++ Fireworks/tgafile.h
@@ -0,0 +1,25 @@
+/*
+   Class Name:
+
+      CTgaFile
+
+   Description:
+
+      load tga file
+*/
+
+#pragma once
+
+class CTgaFile
+{
+public:
+	int width, height;
+	int byteperpixel;
+	char* data;
+
+public:
+	CTgaFile();
+	~CTgaFile();
+
+	bool Load(wchar_t* szFile);
+};
